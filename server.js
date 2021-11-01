15c36d8aa7fd30b65dd41d8cc28d6169c40bab5d
Marvin Rosa <marvin.fortin@outlook.com>
2021-11-01 10:02:58 -0600

    GraphQL Server Running

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const { graphqlHTTP } = require("express-graphql");
+const{
+    GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLString
+}= require('graphql');
+const app = express();
+
+const schema = new GraphQLSchema({
+    query: new GraphQLObjectType({
+        name: 'HelloWorld',
+        fields: () =>({
+            message: {
+                type: GraphQLString,
+                resolve: () => 'Hello World' 
+            }
+        })
+    })
+});
+
+
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+}));
+app.listen(3001,()=>console.log('Server corriendo'));
